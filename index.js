16879b86ba9700b14e6251223e9dd7b28448e9ff
Dominic Tarr <dominic.tarr@gmail.com>
2016-02-21 22:20:11 +1300

    process indexes from array, not hardcoded

--- index.js
+++ index.js
@@ -3,25 +3,38 @@ var pull = require('pull-stream')
 var Write = require('pull-write')
 var pl = require('pull-level')
 var query = require('./query')
+var select = require('./select')
 
 var bytewise = require('bytewise')
 
 var u = require('./util')
 //sorted index.
 
-module.exports = function (path, links, version) {
+function has (key, obj) {
+  return !!obj[key]
+  Object.hasOwnProperty(obj, key)
+}
 
+module.exports = function (path, links, version, codec) {
+  codec = codec || require('bytewise')
   var db = level(path)
 
+  //always write metada to the lowest key,
+  //so the indexes do not interfeer
+  //we don't want to encode this with the codec,
+  //because then we can't change the codec safely
+  //(prehaps the encoding of META is also some indexed value in another codec?)
+  var META = '\x00'
+
   var indexes = [
-    { key: 'SRD', value: ['source', 'rel', 'dest'] },
-    { key: 'DRS', value: ['dest', 'rel', 'source'] },
-    { key: 'RDS', value: ['rel', 'dest', 'source'] }
+    { key: 'SRD', value: ['source', 'rel', 'dest', 'ts'] },
+    { key: 'DRS', value: ['dest', 'rel', 'source', 'ts'] },
+    { key: 'RDS', value: ['rel', 'dest', 'source', 'ts'] }
   ]
 
   return {
     init: function (cb) {
-      db.get('~meta~', function (err, value) {
+      db.get(META, function (err, value) {
         if(value)
           try { value = JSON.parse(value) }
           catch (err) { return cb(null, 0) }
@@ -31,6 +44,7 @@ module.exports = function (path, links, version) {
           
         //if the view has changed, rebuild entire index.
         //else, read current version.
+
         else if(version && value.version !== version)
           level.destroy(path, function (err) {
             if(err) return cb(err)
@@ -48,30 +62,25 @@ module.exports = function (path, links, version) {
         }, function (batch, data) {
           if(!batch)
             batch = [{
-              key: '~meta~',
+              key: META,
               value: {version: version, since: data.ts},
               valueEncoding: 'json',
               type: 'put'
             }]
           function push(ary) {
-            batch.push({key: bytewise.encode(ary).toString('hex'), value: ' ', type: 'put'})
+            batch.push({key: codec.encode(ary), value: ' ', type: 'put'})
           }
-          links(data, function (link) {
-//            push([">", link.source, link.rel, link.dest, data.key])
-//            push(["<", link.dest, link.rel, link.source, data.key])
 
-            //get all relations between two nodes
-            push(["SDR", link.source, link.dest, link.rel, data.ts])
-            //get all relations to a dest
-            push(["DRS", link.dest, link.rel, link.source, data.ts])
-            //get all relations from a source
-            push(["SRD", link.source, link.rel, link.dest, data.ts])
-            //could also have DSR, SRD, RSD
-            //if we expect the user to know two parameters
-            //we only need 3 (say SD SR DR)
-            //if they want a known and a range, then they need 3
-            //only rel makes sense as a range though.
-            //DRS, SRD, SDR
+          links(data, function (link) {
+            indexes.forEach(function (index) {
+              var a = [index.key]
+              for(var i = 0; i < index.value.length; i++) {
+                var key = index.value[i]
+                if(!has(key, link)) return
+                a.push(link[key])
+              }
+              push(a)
+            })
           })
           batch[0].value.since = data.ts
           return batch
@@ -81,28 +90,30 @@ module.exports = function (path, links, version) {
     close: function (cb) {
       db.close(cb)
     },
+    dump: function () {
+      return pl.read(db, {keyEncoding: codec, gt: '\x00'})
+    },
     read: function (opts) {
-      if(!opts) return pl.read(db)
-      if(!opts.query) return pl.read(db, opts)
+      if(!opts) opts = {query: {}}
+      if(!opts.query) opts.query = {}
 
-      console.log(indexes, opts)
-      var opts = query(indexes, opts.query, function (e) {
-        return bytewise.encode(e).toString('hex')
-      })
-      console.log(opts)
+      var index = select(indexes, opts.query)
+      var opts = query(index, opts.query)
+
       opts.values = false
       opts.keys = true
+      opts.keyEncoding = codec
+
       return pull(
         pl.read(db, opts),
+        //this just reads the index, suitable for links.
         pull.map(function (e) {
-          return bytewise.decode(new Buffer(e,'hex'))
+          var o = {}
+          for(var i = 0; i < index.value.length; i++)
+            o[index.value[i]] = e[i+1]
+          return o
         })
       )
     }
   }
 }
-
-
-
-
-
