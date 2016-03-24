f1884e237532a2ed41cf2180128886fb810c5c72
Dominic Tarr <dominic.tarr@gmail.com>
2016-03-24 17:28:40 +1300

    uncomment tests

--- test/query.js
+++ test/query.js
@@ -26,33 +26,33 @@ tape('source and dest are exact', function (t) {
   t.end()
 })
 
-//tape('dest exact, rel is range', function (t) {
-//  var query = select(indexes, {dest: 'bar', rel: {prefix: 'a'}})
-//
-//  t.deepEqual(query, indexes[1])
-//  t.end()
-//})
-//
-//tape('range only', function (t) {
-//  t.deepEqual(select(indexes, {rel: {prefix:'b'}}), indexes[2])
-//  t.end()
-//})
-//
-//tape('all exact', function (t) {
-//  t.deepEqual(select(indexes, {source: 'foo', dest: 'bar', rel:'x' }), indexes[0])
-//  t.end()
-//})
-//
-//tape('all ranges', function (t) {
-//  t.deepEqual(select(indexes, {source: {prefix:'f'}, dest: {prefix:'b'}}), indexes[0])
-//  t.end()
-//})
-//
-//tape('all ranges except rel', function (t) {
-//  t.deepEqual(select(indexes, {source: {prefix:'f'}, dest: {prefix:'b'}, rel: 'x'}), indexes[2])
-//  t.end()
-//})
-//
+tape('dest exact, rel is range', function (t) {
+  var query = select(indexes, {dest: 'bar', rel: {prefix: 'a'}})
+
+  t.deepEqual(query, indexes[1])
+  t.end()
+})
+
+tape('range only', function (t) {
+  t.deepEqual(select(indexes, {rel: {prefix:'b'}}), indexes[2])
+  t.end()
+})
+
+tape('all exact', function (t) {
+  t.deepEqual(select(indexes, {source: 'foo', dest: 'bar', rel:'x' }), indexes[0])
+  t.end()
+})
+
+tape('all ranges', function (t) {
+  t.deepEqual(select(indexes, {source: {prefix:'f'}, dest: {prefix:'b'}}), indexes[0])
+  t.end()
+})
+
+tape('all ranges except rel', function (t) {
+  t.deepEqual(select(indexes, {source: {prefix:'f'}, dest: {prefix:'b'}, rel: 'x'}), indexes[2])
+  t.end()
+})
+
 
 
 
