147b3f47d271ccdc336e1c4432a7ee9144cb1169
Dominic Tarr <dominic.tarr@gmail.com>
2016-03-24 22:13:27 +1300

    get, set, and check a path exists

---
+++ util.js
@@ -0,0 +1,33 @@
+exports.has = function has (key, obj) {
+  if('string' === typeof key)
+    return Object.hasOwnProperty.call(obj, key)
+  for(var i in key) {
+    if(Object.hasOwnProperty.call(obj, key[i]))
+      obj = obj[key[i]]
+    else
+      return false
+  }
+  return true
+}
+
+exports.get = function get (key, obj) {
+  if('string' === typeof key) return obj[key]
+  for(var i in key) {
+    obj = obj[key[i]]
+    if(!obj) return obj
+  }
+  return obj
+}
+
+exports.set = function set(key, value, obj) {
+  if('string' === typeof key)
+    obj[key] = value
+  else {
+    for(var i = 0 ; i < key.length - 1; i++) {
+      obj = (obj[key[i]] = obj[key[i]] || {})
+    }
+    obj[key[key.length -1]] = value
+  }
+}
+
+
