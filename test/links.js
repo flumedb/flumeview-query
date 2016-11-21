e8b9392a66e57a9a39e8d670a9d3e96813c8e5b6
Dominic Tarr <dominic.tarr@gmail.com>
2016-11-21 22:48:40 +0100

    update basic tests to be flumedb

--- test/links.js
+++ test/links.js
@@ -3,52 +3,59 @@ var tape = require('tape')
 var osenv = require('osenv')
 var path = require('path')
 var pull = require('pull-stream')
+var Flume = require('flumedb')
+var FlumeLog = require('flumelog-offset')
 var Links = require('../')
 var rimraf = require('rimraf')
+
+var codec = require('level-codec/lib/encodings')
 
 function all (stream, cb) {
   pull(stream, pull.collect(cb))
 }
 
 var indexes = [
-  { key: 'SRD', value: ['source', 'rel', 'dest', 'ts'] },
-  { key: 'DRS', value: ['dest', 'rel', 'source', 'ts'] },
-  { key: 'RDS', value: ['rel', 'dest', 'source', 'ts'] }
+  { key: 'SRD', value: ['source', 'rel', 'dest'] },
+  { key: 'DRS', value: ['dest', 'rel', 'source'] },
+  { key: 'RDS', value: ['rel', 'dest', 'source'] }
 ]
 
+
+function extract (data, onLink) {
+  for(var k in data.value)
+    onLink({source: data.key, dest: data.value[k], rel: k, ts: data.ts})
+}
 
 tape('simple', function (t) {
   var linksPath = path.join(osenv.tmpdir(), 'test_stream-view_links')
   rimraf.sync(linksPath)
 
-  function extract (data, onLink) {
-    for(var k in data.value)
-      onLink({source: data.key, dest: data.value[k], rel: k, ts: data.ts})
-  }
+  var db = Flume(FlumeLog(path.join(linksPath, 'log.offset'), 1024, codec.json))
+            .use('links', Links(indexes, extract))
 
   var data = [
-    {key: 'START', value: {read: 'READY', error: 'ERROR', end: 'END'}, ts: 1},
+    {key: 'START', value: {read: 'READY', error: 'ERROR', end: 'END'}},
     {key: 'READY', value: {
-      read: 'START', error: "ERROR", end: "END"}, ts: 2
+      read: 'START', error: "ERROR", end: "END"}
     },
-    {key: 'ERROR', value: {}, ts: 3},
-    {key: 'END', value: {error: 'END'}, ts: 4},
+    {key: 'ERROR', value: {}},
+    {key: 'END', value: {error: 'END'}},
   ]
 
-  var links = Links(linksPath, indexes, extract, 1)
+  var links = db.links
 
   t.test('init', function (t) {
-    links.init(function (err, since) {
-      if(err) throw err
-      t.notOk(since)
+    links.since.once(function (v) {
+      t.equal(v, -1)
       t.end()
+
     })
   })
 
   var live = []
 
   pull(
-    links.read({query: [{$filter: {rel: {$prefix: 'e'}}}], live: true}),
+    links.read({query: [{$filter: {rel: {$prefix: 'e'}}}], live: true, sync: false}),
     pull.drain(function (e) {
       console.log('LIVE', e)
       live.push(e)
@@ -56,13 +63,11 @@ tape('simple', function (t) {
   )
 
   t.test('load', function (t) {
-    pull(
-      pull.values(data),
-      links.write(function (err) {
-        if(err) throw err
-        t.end()
-      })
-    )
+    console.log(data)
+    db.append(data, function (err) {
+      if(err) throw err
+      t.end()
+    })
   })
 
   t.test('query', function (t) {
@@ -70,46 +75,7 @@ tape('simple', function (t) {
       if(err) throw err
       console.log(ary)
 //      t.equal(ary.length, 22)
-      all(links.dump({
-        gte: ['DRS', 'START', 'read', '!'],
-        lt: ['DRS', 'START', 'read', '~', undefined]
-      }), function (err, ary) {
-        console.log(ary)
-        t.end()
-      })
-    })
-  })
-
-  t.test('reinitialize', function (t) {
-    links.close(function (err) {
-      if(err) throw err
-      console.log(indexes)
-      links = Links(linksPath, indexes, extract, 2)
-      links.init(function (err, since) {
-        t.notOk(since)
-        pull(
-          pull.values(data),
-          links.write(function (err) {
-            all(links.read(), function (err, ary) {
-              if(err) throw err
-              console.log(ary)
-              t.end()
-            })
-          })
-        )
-      })
-    })
-  })
-
-  t.test('catchup', function (t) {
-    links.close(function (err) {
-      if(err) throw err
-      links = Links(linksPath, indexes, extract, 2)
-      links.init(function (err, since) {
-        if(err) throw err
-        t.equal(since, 4)
-        t.end()
-      })
+      t.end()
     })
   })
 
@@ -124,20 +90,28 @@ tape('simple', function (t) {
 
   t.test('live', function (t) {
     t.deepEqual(live, [{
-      dest: 'ERROR', rel: 'error', source: 'START', ts: 1
+      dest: 'ERROR', rel: 'error', source: 'START'
     }, {
-      dest: 'END', rel: 'end', source: 'START', ts: 1
+      dest: 'END', rel: 'end', source: 'START'
     }, {
-      dest: 'ERROR', rel: 'error', source: 'READY', ts: 2
+      dest: 'ERROR', rel: 'error', source: 'READY'
     }, {
-      dest: 'END', rel: 'end', source: 'READY', ts: 2
+      dest: 'END', rel: 'end', source: 'READY'
     }, {
-      dest: 'END', rel: 'error', source: 'END', ts: 4
+      dest: 'END', rel: 'error', source: 'END'
     }])
 
     t.end()
   })
 })
+
+
+
+
+
+
+
+
 
 
 
