aa2a78d0050d935058d23d25e4788e6d608b4c5c
Dominic Tarr <dominic.tarr@gmail.com>
2016-11-24 11:07:20 +0100

    set LO and HI bounds (for seq)

--- query.js
+++ query.js
@@ -14,23 +14,25 @@ module.exports = function (index, query) {
     )
   }
 
-  function build (index, map) {
+  function build (index, map, b) {
     var a = [index.key]
     for(var i = 0; i < index.value.length; i++)
       a.push(map(get(index.value[i], query)))
+    a.push(b)
     return a
   }
 
   return {
     gte: build(index, function (value) {
       return bound(value, Q.lower, Q.LO)
-    }),
+    }, Q.LO),
     lte: build(index, function (value) {
       return bound(value, Q.upper, Q.HI)
-    })
+    }, Q.HI)
     //reverse, limit, live?
   }
 
 }
+
 
 
